935bc2e7b48d7e18ff988247c3ccf9b4b9b44d01
cleberpires32 <cleberpires269@gmail.com>
2022-06-02 22:24:12 -0300

    criação da primeira carda de dados no bd h2

--- src/main/java/com/cleber/helpDeskapi/HelpDeskApplication.java
+++ src/main/java/com/cleber/helpDeskapi/HelpDeskApplication.java
@@ -1,13 +1,50 @@
 package com.cleber.helpDeskapi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelpDeskApplication {
+import com.cleber.helpDeskapi.domain.Chamado;
+import com.cleber.helpDeskapi.domain.Cliente;
+import com.cleber.helpDeskapi.domain.Tecnico;
+import com.cleber.helpDeskapi.domain.enums.Perfil;
+import com.cleber.helpDeskapi.domain.enums.Prioridade;
+import com.cleber.helpDeskapi.domain.enums.Status;
+import com.cleber.helpDeskapi.repository.ChamadoRepository;
+import com.cleber.helpDeskapi.repository.ClienteRepository;
+import com.cleber.helpDeskapi.repository.TecnicoRepository;
 
+@SpringBootApplication
+public class HelpDeskApplication implements CommandLineRunner {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpDeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tc1 = new Tecnico(null, "Cleber Pires", "cleber@gmail.com", "11683800060", "123585adm");
+		tc1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cl1 = new Cliente(null, "Sidney Alves", "sidney@gmail.com", "54811402090", "5644563cli");
+		
+		Chamado ch1 = new Chamado(null, "Primeira carga", "Chamado 1", Prioridade.MEDIA, Status.ANDAMENTO, tc1, cl1);
+	
+		tecnicoRepository.saveAll(Arrays.asList(tc1));
+		clienteRepository.saveAll(Arrays.asList(cl1));
+		chamadoRepository.saveAll(Arrays.asList(ch1));
 	}
 
 }
